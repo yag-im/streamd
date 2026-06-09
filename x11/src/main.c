070b1c824a260a02f131b056c7a3076e5c97b1a2
Robert Ayrapetyan <robert@yag.im>
2026-06-09 19:31:03 +0000

    fix: final nvidia piepline fixes

--- x11/src/main.c
+++ x11/src/main.c
@@ -6,14 +6,14 @@
 #include "utils.h"
 
 struct StreamDCtx {
-    GMainLoop *loop;
-    GstElement *pipeline;
-    struct NaviCapture *navi_capture;
+    GMainLoop* loop;
+    GstElement* pipeline;
+    struct NaviCapture* navi_capture;
 } g_ctx = {.loop = NULL, .pipeline = NULL, .navi_capture = NULL};
 
-static gboolean on_bus_message(GstBus *bus, GstMessage *message, GMainLoop *loop) {
-    GError *err;
-    gchar *debug_info;
+static gboolean on_bus_message(GstBus* bus, GstMessage* message, GMainLoop* loop) {
+    GError* err;
+    gchar* debug_info;
     switch (GST_MESSAGE_TYPE(message)) {
         case GST_MESSAGE_EOS:
             log_info("finished playback");
@@ -34,9 +34,9 @@ static gboolean on_bus_message(GstBus *bus, GstMessage *message, GMainLoop *loop
     return TRUE;
 }
 
-gchar *compose_gstreamer_pipeline() {
+gchar* compose_gstreamer_pipeline() {
     // based on: https://github.com/selkies-project/selkies-gstreamer/blob/main/src/selkies_gstreamer/gstwebrtc_app.py
-    gchar *pipeline_str =
+    gchar* pipeline_str =
         "\
         ximagesrc display-name=%s show-pointer=%s use-damage=false remote=true blocksize=16384\
             ! video/x-raw,framerate=%s/1\
@@ -45,7 +45,8 @@ gchar *compose_gstreamer_pipeline() {
     if (VIDEO_ENC == "gpu-intel") {
         pipeline_str = g_strconcat(pipeline_str,
                                    "\
-            ! qsvh264enc bitrate=10000 low-latency=true target-usage=7",
+            ! qsvh264enc bitrate=10000 low-latency=true target-usage=7\
+            ! video/x-h264,stream-format=byte-stream,profile=constrained-baseline",
                                    NULL);
     } else if (VIDEO_ENC == "gpu-nvidia") {
         pipeline_str = g_strconcat(pipeline_str,
@@ -53,27 +54,31 @@ gchar *compose_gstreamer_pipeline() {
             ! cudaupload\
             ! cudaconvert qos=true\
             ! video/x-raw(memory:CUDAMemory),format=NV12\
-            ! nvh264enc name=nvenc bitrate=10000 rc-mode=cbr gop-size=-1 strict-gop=true aud=false b-adapt=false rc-lookahead=0 bframes=0 cabac=true repeat-sequence-header=true preset=p4 tune=ultra-low-latency multi-pass=two-pass-quarter",
+            ! nvh264enc name=nvenc bitrate=10000 rc-mode=cbr gop-size=-1 strict-gop=true aud=false b-adapt=false rc-lookahead=0 bframes=0 cabac=true repeat-sequence-header=true preset=p4 tune=ultra-low-latency multi-pass=two-pass-quarter\
+            ! video/x-h264,stream-format=avc,profile=constrained-baseline",
                                    NULL);
     } else if (VIDEO_ENC == "cpu") {
+        // stream-format=byte-stream breaks nvidia pipeline
+        // (https://discourse.gstreamer.org/t/h264parse-never-receives-valid-picture-headers/3819)
         pipeline_str = g_strconcat(pipeline_str,
                                    "\
-            ! x264enc bitrate=10000 tune=zerolatency speed-preset=ultrafast threads=2 key-int-max=2560 b-adapt=false bframes=0 b-pyramid=false vbv-buf-capacity=120 pass=cbr",
+            ! x264enc bitrate=10000 tune=zerolatency speed-preset=ultrafast threads=2 key-int-max=2560 b-adapt=false bframes=0 b-pyramid=false vbv-buf-capacity=120 pass=cbr\
+            ! video/x-h264,stream-format=byte-stream,profile=constrained-baseline",
                                    NULL);
     } else {
         log_error("unrecognized VIDEO_ENC: %s", VIDEO_ENC);
         return NULL;
     }
     // constrained-baseline profile generates:
-    // for cpu: na=fmtp:96 packetization-mode=1;profile-level-id=42c01f;level-asymmetry-allowed=1
-    // for intel-gpu: na=fmtp:96 packetization-mode=1;profile-level-id=424028;level-asymmetry-allowed=1
+    // for cpu:        na=fmtp:96 packetization-mode=1;profile-level-id=42c01f;level-asymmetry-allowed=1
+    // for intel-gpu:  na=fmtp:96 packetization-mode=1;profile-level-id=424028;level-asymmetry-allowed=1
+    // for nvidia-gpu:            packetization-mode=1;profile-level-id=42e01f;level-asymmetry-allowed=1
     // which are compatible with both chrome and firefox
-    // baseline and main profile generate profile-level-ids which are not supported in Firefox in cpu and/or intel-gpu
-    // modes do not add stream-format=byte-stream into the caps as it breaks nvidia pipeline
-    // (https://discourse.gstreamer.org/t/h264parse-never-receives-valid-picture-headers/3819)
+    // baseline and main profiles generate profile-level-ids which are not supported by Firefox in cpu and intel-gpu
+    // modes.
+
     pipeline_str = g_strconcat(pipeline_str,
                                "\
-            ! video/x-h264,profile=constrained-baseline \
             ! queue\
             ! wrs.\
         pulsesrc\
@@ -97,30 +102,30 @@ gchar *compose_gstreamer_pipeline() {
 
 void play_stream() {
     log_info("play_stream");
-    GError *error = NULL;
-    gchar *pipeline_str = compose_gstreamer_pipeline();
+    GError* error = NULL;
+    gchar* pipeline_str = compose_gstreamer_pipeline();
     if (!pipeline_str) exit(1);
     log_info("pipeline: %s", pipeline_str);
     g_ctx.pipeline = gst_parse_launch(pipeline_str, &error);
     if (!g_ctx.pipeline) {
         log_error("parse error: %s", error->message);
     }
-    GstBus *bus = gst_element_get_bus(g_ctx.pipeline);
+    GstBus* bus = gst_element_get_bus(g_ctx.pipeline);
     if (!bus) {
         log_error("bus error");
     }
     gst_bus_add_watch(bus, (GstBusFunc)on_bus_message, g_ctx.loop);
     gst_object_unref(bus);
 
-    GstElement *src = gst_bin_get_by_name(GST_BIN(g_ctx.pipeline), "ximagesrc0");
+    GstElement* src = gst_bin_get_by_name(GST_BIN(g_ctx.pipeline), "ximagesrc0");
     if (!src) {
         log_error("pipeline error");
     }
-    GstPad *pad = gst_element_get_static_pad(src, "src");
+    GstPad* pad = gst_element_get_static_pad(src, "src");
     gst_pad_add_probe(pad, GST_PAD_PROBE_TYPE_EVENT_UPSTREAM, g_ctx.navi_capture->event_probe_cb, g_ctx.navi_capture,
                       NULL);
 
-    GstElement *overlay = gst_bin_get_by_name(GST_BIN(g_ctx.pipeline), "overlay");
+    GstElement* overlay = gst_bin_get_by_name(GST_BIN(g_ctx.pipeline), "overlay");
     if (!overlay) {
         log_error("Failed to get textoverlay element.\n");
     }
@@ -136,12 +141,12 @@ void play_stream() {
     return;
 }
 
-gint main(gint argc, gchar *argv[]) {
+gint main(gint argc, gchar* argv[]) {
     setvbuf(stdout, NULL, _IONBF, 0);  // flush after printf
 
     // first we need to initialize navigation (mouse, keyboard) events handler
     // init_navi_capture() returns NaviCapture which we store and use later from the play_stream()
-    struct NaviCapture *n_capture;
+    struct NaviCapture* n_capture;
     if (!init_navi_capture(&n_capture)) log_error("navi events handler init error");
     g_ctx.navi_capture = n_capture;
 
